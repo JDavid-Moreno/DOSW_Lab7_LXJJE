ef2d453e807fef88f45c0358207f13754f011f27
unknown <juan.mdaleman@labinfo.is.escuelaing.edu.co>
2026-03-11 17:10:56 -0500

    feature: create spring boot with maven

---
+++ TechCup/TechCup/src/test/java/edu/eci/dosw/TechCup/TechCupApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.TechCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

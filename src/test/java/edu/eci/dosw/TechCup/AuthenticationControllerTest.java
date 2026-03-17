05034ae65f8be2ab02ae5cba6a8061435745a288
JDmorenoo0320 <davidmorenodaleman@gmail.com>
2026-03-17 18:21:05 -0500

    feature: pom and add controller test

---
+++ src/test/java/edu/eci/dosw/TechCup/AuthenticationControllerTest.java
@@ -0,0 +1,56 @@
+package edu.eci.dosw.TechCup;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.eci.dosw.TechCup.model.User;
+import edu.eci.dosw.TechCup.service.AuthenticationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthenticationController.class)
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldRegisterUser() throws Exception {
+        User user = new User();
+        user.setEmail("test@mail.com");
+        user.setPassword("123");
+
+        when(authenticationService.register(user)).thenReturn(user);
+        mockMvc.perform(post("/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("test@mail.com"));
+    }
+
+    @Test
+    void shouldLoginUser() throws Exception {
+        User user = new User();
+        user.setEmail("test@mail.com");
+        user.setPassword("123");
+
+        when(authenticationService.login("test@mail.com", "123")).thenReturn(user);
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("test@mail.com"));
+    }
+}

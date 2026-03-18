0aa69db38087d8f46e9e3e36d68f5edeb15e4c69
EduardoRico26 <edurico0621@gmail.com>
2026-03-18 18:24:03 -0500

    framework de logs en TournamentServiceProd.java

--- src/main/java/edu/eci/dosw/TechCup/service/TournamentServiceProd.java
+++ src/main/java/edu/eci/dosw/TechCup/service/TournamentServiceProd.java
@@ -12,11 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @Profile("prod")
 public class TournamentServiceProd implements TournamentService {
     private TournamentRepository tournamentRepository;
+    private static final Logger log = LoggerFactory.getLogger(TournamentServiceProd.class);
 
     public TournamentServiceProd(TournamentRepository tournamentRepository) {
         this.tournamentRepository = tournamentRepository;

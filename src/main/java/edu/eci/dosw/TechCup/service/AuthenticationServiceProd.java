e712bd4d5550ce4fde41598277ff8362d3173aaf
EduardoRico26 <edurico0621@gmail.com>
2026-03-19 10:35:05 -0500

    Corrección AuthenticationServiceProd.java

--- src/main/java/edu/eci/dosw/TechCup/service/AuthenticationServiceProd.java
+++ src/main/java/edu/eci/dosw/TechCup/service/AuthenticationServiceProd.java
@@ -9,15 +9,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @Service
 @Profile("prod")
 public class AuthenticationServiceProd implements AuthenticationService {
     private PasswordEncoder encoder;
     private UserRepository userRepository;
-    private static final Logger log = LoggerFactory.getLogger(AuthenticationServiceProd.class);
+    
 
     public AuthenticationServiceProd(PasswordEncoder encoder, UserRepository userRepository) {
         this.encoder = encoder;

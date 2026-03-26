acc2d13b2f07c89d9c8ec7333b85151a1edaf449
unknown <juan.mdaleman@labinfo.is.escuelaing.edu.co>
2026-03-26 12:54:51 -0500

    feature: correction TeamInvitationServicesProd

--- src/main/java/edu/eci/dosw/TechCup/service/TeamInvitationServiceProd.java
+++ src/main/java/edu/eci/dosw/TechCup/service/TeamInvitationServiceProd.java
@@ -1,69 +1,73 @@
 package edu.eci.dosw.TechCup.service;
 
+import edu.eci.dosw.TechCup.entity.TeamEntity;
+import edu.eci.dosw.TechCup.entity.TeamInvitationEntity;
+import edu.eci.dosw.TechCup.entity.TeamMemberEntity;
+import edu.eci.dosw.TechCup.entity.UserEntity;
+import edu.eci.dosw.TechCup.exception.TeamInvitationException;
+import edu.eci.dosw.TechCup.mapper.TeamInvitationMapper;
+import edu.eci.dosw.TechCup.model.InvitationStatus;
+import edu.eci.dosw.TechCup.model.Role;
 import edu.eci.dosw.TechCup.model.Team;
 import edu.eci.dosw.TechCup.model.TeamInvitation;
-import edu.eci.dosw.TechCup.model.User;
-import edu.eci.dosw.TechCup.model.Role;
-import edu.eci.dosw.TechCup.repository.TeamRepository;
 import edu.eci.dosw.TechCup.repository.TeamInvitationRepository;
+import edu.eci.dosw.TechCup.repository.TeamMemberRepository;
 import edu.eci.dosw.TechCup.repository.UserRepository;
-import edu.eci.dosw.TechCup.entity.TeamInvitationEntity;
-import edu.eci.dosw.TechCup.entity.TeamEntity;
-import edu.eci.dosw.TechCup.entity.UserEntity;
-import edu.eci.dosw.TechCup.mapper.TeamInvitationMapper;
+import edu.eci.dosw.TechCup.service.TeamInvitationService;
 import org.springframework.stereotype.Service;
-import edu.eci.dosw.TechCup.exception.TeamInvitationException;
 import org.springframework.transaction.annotation.Transactional;
 
 
 
 @Service
 public class TeamInvitationServiceProd implements TeamInvitationService {
-    private TeamInvitationRepository teamInvitationRepository;
-    private TeamInvitationMapper teamInvitationMapper;
-    private UserRepository userRepository;
+    private final TeamInvitationRepository invitationRepository;
+    private final TeamInvitationMapper mapper;
+    private final UserRepository userRepository;
+    private final TeamMemberRepository teamMemberRepository;
 
-    public TeamInvitationServiceProd(TeamInvitationRepository teamInvitationRepository, TeamInvitationMapper teamInvitationMapper, UserRepository userRepository) {
-        this.teamInvitationRepository = teamInvitationRepository;
-        this.teamInvitationMapper = teamInvitationMapper;
+
+    public TeamInvitationServiceProd(TeamInvitationRepository invitationRepository,
+                                     TeamInvitationMapper mapper, 
+                                    UserRepository userRepository, 
+                                    TeamMemberRepository teamMemberRepository) {
+        this.invitationRepository = invitationRepository;
+        this.mapper = mapper;
         this.userRepository = userRepository;
+        this.teamMemberRepository = teamMemberRepository;
     }
 
+    @Override
     @Transactional
     public TeamInvitation inviteTeam(Long id, Long invitedId) {
-        TeamEntity team = teamInvitationRepository.findAllByInvitedId(invitedId)
-            .orElseThrow(() -> new TeamInvitationException(TeamInvitationException.TEAM_NOT_FOUND));
-        UserEntity user = userRepository.findById(id)
+        UserEntity invited = userRepository.findById(invitedId)
             .orElseThrow(() -> new TeamInvitationException(TeamInvitationException.USER_NOT_FOUND));
-        if (user.getRole() == Team.Role.JUGADOR) {
-            throw new TeamInvitationException("User is not a team member");
-            }
-        TeamInvitationEntity entity = new TeamInvitationEntity();
-        entity.setInvited(user);
-        entity.setInvitedBy(team);
-        entity.setStatus(TeamInvitationEntity.InvitationStatus.PENDING);
-        return teamInvitationMapper.toModel(teamInvitationRepository.save(entity));
-    }
+        TeamMemberEntity invitedBy = teamMemberRepository.findById(invitedId)
+            .orElseThrow(() -> new TeamInvitationException(TeamInvitationException.TEAM_NOT_FOUND));
+        if (invited.getRole() != Role.JUGADOR) {
+            throw new TeamInvitationException(TeamInvitationException.ONLY_PLAYERS_INVITED);
+        }
 
+        TeamInvitationEntity entity = new TeamInvitationEntity();
+        entity.setInvited(invited);
+        entity.setInvitedBy(invitedBy);
+
+        return mapper.toModel(invitationRepository.save(entity));
+    }   
+
+    @Override
     @Transactional
     public TeamInvitation acceptInvitation(Long id) {
-        TeamInvitationEntity entity = teamInvitationRepository.findById(id)
+        TeamInvitationEntity invitation = invitationRepository.findById(id)
             .orElseThrow(() -> new TeamInvitationException(TeamInvitationException.INVITATION_NOT_FOUND));
-        if (entity.getStatus() != TeamInvitationEntity.InvitationStatus.PENDING) {
-            throw new TeamInvitationException("Invitation is not pending");
-        }
-        entity.setStatus(TeamInvitationEntity.InvitationStatus.ACCEPTED);
-        return teamInvitationMapper.toModel(teamInvitationRepository.save(entity));
+        return mapper.toModel(invitationRepository.save(invitation));
     }
 
     @Transactional
     public TeamInvitation rejectInvitation(Long id) {
-        TeamInvitationEntity entity = teamInvitationRepository.findById(id)
+        TeamInvitationEntity invitation = invitationRepository.findById(id)
             .orElseThrow(() -> new TeamInvitationException(TeamInvitationException.INVITATION_NOT_FOUND));
-        if (entity.getStatus() != TeamInvitationEntity.InvitationStatus.PENDING) {
-            throw new TeamInvitationException("Invitation is not pending");
-        }
-        entity.setStatus(TeamInvitationEntity.InvitationStatus.REJECTED);
-        return teamInvitationMapper.toModel(teamInvitationRepository.save(entity));
+        
+       return mapper.toModel(invitationRepository.save(invitation));
     }
 }

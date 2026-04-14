6f2a35cdc022c5f06196a76a5c718f8540eab6f2
JDmorenoo0320 <davidmorenodaleman@gmail.com>
2026-04-14 00:22:34 -0500

    feature: correct class

--- src/test/java/edu/eci/dosw/TechCup/controller/UserControllerTest.java
+++ src/test/java/edu/eci/dosw/TechCup/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package edu.eci.dosw.TechCup.controller;
 
-import edu.eci.dosw.TechCup.model.Role;
 import edu.eci.dosw.TechCup.model.User;
 import edu.eci.dosw.TechCup.model.UserState;
 import edu.eci.dosw.TechCup.service.UserService;
@@ -12,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
@@ -19,9 +19,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
@@ -40,7 +37,7 @@ class UserControllerTest {
 
         ResponseEntity<User> response = controller.get(1L);
 
-        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
     }
 
@@ -51,30 +48,18 @@ class UserControllerTest {
 
         ResponseEntity<User> response = controller.get(1L);
 
-        assertEquals(404, response.getStatusCodeValue());
-    }
-
-    @Test
-    void shouldAddUserRole() {
-        doNothing().when(userService).addRole(1L, 0L);
-
-        ResponseEntity<User> response = controller.addRole(1L, 0L);
-
-        assertEquals(204, response.getStatusCodeValue());
-        verify(userService, times(1)).addRole(1L, 0L);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
     void shouldUpdateUserState() {
-        UserState state = UserState.ACTIVE; 
+        UserState state = UserState.ACTIVE;
 
         doNothing().when(userService).updateState(1L, state);
 
         ResponseEntity<User> response = controller.updateState(1L, state);
 
-        assertEquals(204, response.getStatusCodeValue());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
         verify(userService, times(1)).updateState(1L, state);
     }
-} 
-    
-
+}

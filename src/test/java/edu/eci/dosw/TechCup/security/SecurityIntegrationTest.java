535deb888775551d7a837e65a065889c83c8867f
Karol-Reyes <karol.rodriguez-r@mail.escuelaing.edu.co>
2026-04-13 21:56:16 -0500

    feature/ new tests initial configuration

--- src/test/java/edu/eci/dosw/TechCup/security/SecurityIntegrationTest.java
+++ src/test/java/edu/eci/dosw/TechCup/security/SecurityIntegrationTest.java
@@ -1,12 +1,43 @@
 package edu.eci.dosw.TechCup.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.eci.dosw.TechCup.model.AuthRequest;
+import edu.eci.dosw.TechCup.model.User;
+import edu.eci.dosw.TechCup.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("prod")
 public class SecurityIntegrationTest {
-    
+@Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setup() {
+        userRepository.deleteAll();
+    }
 }

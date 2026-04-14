6b684bea3e2db57292b614fe45592aadd4c4722b
Karol-Reyes <karol.rodriguez-r@mail.escuelaing.edu.co>
2026-04-13 21:54:29 -0500

    feature/ new tests

---
+++ src/test/java/edu/eci/dosw/TechCup/security/SecurityIntegrationTest.java
@@ -0,0 +1,12 @@
+package edu.eci.dosw.TechCup.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class SecurityIntegrationTest {
+    
+}

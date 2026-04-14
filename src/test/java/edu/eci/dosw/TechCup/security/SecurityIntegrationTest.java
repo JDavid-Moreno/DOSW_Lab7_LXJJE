4f87eeaf3ce9cacbe574304c35266bc75456dd76
Karol-Reyes <karol.rodriguez-r@mail.escuelaing.edu.co>
2026-04-13 23:29:54 -0500

    feature/fix initial configuration

--- src/test/java/edu/eci/dosw/TechCup/security/SecurityIntegrationTest.java
+++ src/test/java/edu/eci/dosw/TechCup/security/SecurityIntegrationTest.java
@@ -1,43 +1,55 @@
 package edu.eci.dosw.TechCup.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.eci.dosw.TechCup.model.AuthRequest;
-import edu.eci.dosw.TechCup.model.User;
-import edu.eci.dosw.TechCup.repository.UserRepository;
+import com.jayway.jsonpath.JsonPath;
+import edu.eci.dosw.TechCup.mapper.RoleMapper;
+import edu.eci.dosw.TechCup.service.JwtService;
+import edu.eci.dosw.TechCup.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("prod")
 public class SecurityIntegrationTest {
-@Autowired
+
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @MockBean
+    private RoleMapper roleMapper;
 
-    @Autowired
-    private UserRepository userRepository;
+    @MockBean
+    private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    @MockBean
+    private UserDetailsService userDetailsService;
 
-    @BeforeEach
-    void setup() {
-        userRepository.deleteAll();
-    }
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private UserService userService;
+
 }
